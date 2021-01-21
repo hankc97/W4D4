edbe2225f2a29f00d189efd47e817fe7b723a716
alejandro <alejandroweil13@gmail.com>
2021-01-21 14:43:59 -0500

    Wrote #two_sum and #my_tranpose

--- first_TDD/lib/enumerables.rb
+++ first_TDD/lib/enumerables.rb
@@ -1,4 +1,23 @@
 def my_uniq(array)
     array.uniq
+end
 
+def two_sum(array)
+    new_arr = []
+    (0...array.length).each do |idx1|
+        (idx1 + 1...array.length).each do |idx2|
+            new_arr << [idx1, idx2] if array[idx1] + array[idx2] == 0
+        end
+    end
+    new_arr
+end
+
+def my_tranpose(matrix)
+    new_matrix = Array.new(matrix.length) {Array.new(matrix.length)}
+    matrix.each_index do |idx1|
+        matrix.each_index do |idx2|
+            new_matrix[idx1][idx2] = matrix[idx2][idx1]
+        end
+    end
+    new_matrix
 end
--- first_TDD/spec/enumerables_spec.rb
+++ first_TDD/spec/enumerables_spec.rb
@@ -8,3 +8,28 @@ describe "#my_uniq" do
     end
 
 end
+
+describe "#two_sum" do
+    let(:array) {[-1 ,0 ,2 ,-2 ,1]}
+    it "returns 2D array of indexes that equal to 0" do
+        expect(two_sum(array)).to eq([[0, 4], [2, 3]])
+    end
+
+
+end
+
+describe "#my_tranpose" do
+    let(:matrix) { [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+      ]}
+    let (:tranposed) {[
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8]
+      ]}  
+    it "returns transposed matrix" do
+        expect(my_tranpose(matrix)).to eq(tranposed)
+    end
+end
